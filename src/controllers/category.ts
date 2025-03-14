3b9aaad30bba7108ee3b6417b76bf711bf430f9b
safin-sys <mdsafinofficial@gmail.com>
2025-03-14 16:52:35 +0600

    Refactor category creation logic to handle subcategories and improve error handling

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -6,56 +6,57 @@ const add_category = async (req: Request, res: Response) => {
   const { parent, name, details } = req?.body || {};
   const image = req.file;
 
-  const category = await Category.findOne({ name });
+  if (!parent) {
+    // create categories
+    const category = await Category.findOne({ name });
 
-  if (category && !parent) {
-    res.status(400).json({
-      message: `Category ${name} already exists`,
-    });
-    return;
-  }
+    if (category && !category.subcategory_of) {
+      res.status(400).json({
+        message: `Category ${name} already exists`,
+      });
+      return;
+    }
 
-  let parentId;
-
-  if (parent) {
-    parentId = await Category.findOne({ name: parent });
-
+    try {
+      const img_url = await uploadService(image, "image");
+      await Category.create({ name, details, img_url });
+      res.json({ message: "Category created successfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error", error });
+    }
+  } else {
+    // create subcategories
+    const parentId = await Category.findOne({ name: parent });
     if (!parentId) {
       res.status(400).json({
         message: `Invalid parent name`,
       });
       return;
-    } else {
-      const subcategory = await Category.findOne({
-        name,
-        subcategory_of: parentId,
-      });
-      if (subcategory) {
-        res.status(400).json({
-          message: `Subcategory ${name} already exists`,
-        });
-        return;
-      }
-      try {
-        const img_url = await uploadService(image, "image");
-        await Category.create({ name, subcategory_of: parentId, img_url });
-        res.json({ message: "Subcategory created successfully" });
-        return;
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Internal Server Error" });
-        return;
-      }
     }
-  }
 
-  try {
-    const img_url = await uploadService(image, "image");
-    await Category.create({ name, details, img_url });
-    res.json({ message: "Category created successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error", error });
+    const subcategory = await Category.findOne({
+      name,
+      subcategory_of: parentId,
+    });
+
+    if (subcategory) {
+      res.status(400).json({
+        message: `Subcategory ${name} already exists`,
+      });
+      return;
+    }
+
+    try {
+      const img_url = await uploadService(image, "image");
+      await Category.create({ name, subcategory_of: parentId, img_url });
+      res.json({ message: "Subcategory created successfully" });
+      return;
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+      return;
+    }
   }
 };
 
